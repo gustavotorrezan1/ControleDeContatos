8772ca2208e1d60c51e5e5f1750f3624b2f20ee6
gustavotorrezan1 <gustorrezani@gmail.com>
2023-08-15 11:59:29 -0300

    .

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -7,12 +7,12 @@ public class ContatoModel
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Digite o nome do contato")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
 
     [Required(ErrorMessage = "Digite o celular do contato")]
     [Phone(ErrorMessage = "O celular informado não é valido!")]
-    public string Celular { get; set; }
+    public string? Celular { get; set; }
 
     [Required(ErrorMessage = "Digite o email do contato")]
-    public string Email { get; set; }
+    public string? Email { get; set; }
 }
--- ControleDeContatos/Program.cs
+++ ControleDeContatos/Program.cs
@@ -4,22 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// String de conecção
+// String de conexão
 
-//builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
+/*
 string mySqlConnection =
     builder.Configuration.GetConnectionString("Online");
+
 
 builder.Services.AddDbContextPool<BancoContext>(options =>
     options.UseMySql(mySqlConnection,
         ServerVersion.AutoDetect(mySqlConnection)));
-
+*/
 builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
-
-//builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer("Server=aws.connect.psdb.cloud;Database=db_sistemacontatos;user=civ1axn795le27p5dlgc;password=pscale_pw_M6eTZwXsFB4mBnyXoyv9e2fIXGfn0g1dk8vdfNUfKns;S"));
-//builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
